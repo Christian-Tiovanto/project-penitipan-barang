b643192a6dd0748cf6efdf9dbe1ab5e33a4ee0d0
Christian-Tiovanto <christiantiovanto1@gmail.com>
2025-06-23 00:55:27 +0700

    refactor: use raw sql query instead of typeorm

--- src/modules/transaction-in/services/transaction-in-header.service.ts
+++ src/modules/transaction-in/services/transaction-in-header.service.ts
@@ -1,5 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { TransactionInHeader } from '../models/transaction-in-header.entity';
 import { Customer } from '@app/modules/customer/models/customer.entity';
 import {
@@ -10,8 +14,13 @@ import {
 import { UpdateTransactionInHeaderDto } from '../dtos/update-trans-in-header.dto';
 import { CustomerService } from '@app/modules/customer/services/customer.service';
 import { DATABASE } from '@app/enums/database-table';
-import { TransactionInHeaderColumn } from '@app/enums/table-column';
-import { PoolClient } from 'pg';
+import {
+  CustomersColumn,
+  TransactionInHeaderColumn,
+  TransactionInsColumn,
+} from '@app/enums/table-column';
+import { Pool, PoolClient } from 'pg';
+import { DATABASE_POOL } from '@app/modules/database/database.module';
 interface GetAllTransactionInHeaderQuery {
   pageNo: number;
   pageSize: number;
@@ -24,8 +33,7 @@ interface GetAllTransactionInHeaderQuery {
 @Injectable()
 export class TransactionInHeaderService {
   constructor(
-    // @InjectRepository(TransactionInHeader)
-    // private transactionInHeaderRepository: Repository<TransactionInHeader>,
+    @Inject(DATABASE_POOL) private readonly pool: Pool,
     private readonly customerService: CustomerService,
   ) {}
 
@@ -56,73 +64,117 @@ export class TransactionInHeaderService {
     return rows[0];
   }
 
-  // async getAllTransactionInHeader({
-  //   pageNo,
-  //   pageSize,
-  //   sort,
-  //   order,
-  //   startDate,
-  //   endDate,
-  //   search,
-  // }: GetAllTransactionInHeaderQuery) {
-  //   const skip = (pageNo - 1) * pageSize;
+  async getAllTransactionInHeader({
+    pageNo,
+    pageSize,
+    sort,
+    order,
+    startDate,
+    endDate,
+    search,
+  }: GetAllTransactionInHeaderQuery) {
+    const values: any[] = [];
+    let paramIndex = 1;
+    const whereConditions = [];
+    let sortBy: string = `th.${sort}`;
+    if (sort === TransactionInHeaderSort.CUSTOMER) {
+      sortBy = `MIN(${sort}.name)`;
+    }
+    if (sort === TransactionInHeaderSort.IS_CHARGE) {
+      sortBy = `th.${sort}`;
+    }
 
-  //   let sortBy: string = `transaction-header.${sort}`;
-  //   if (sort === TransactionInHeaderSort.CUSTOMER) {
-  //     sortBy = `${sort}.name`;
-  //   }
+    if (startDate) {
+      whereConditions.push(`th.created_at >= $${paramIndex++}`);
+      values.push(startDate);
+    }
+    if (endDate) {
+      whereConditions.push(`th.created_at  < $${paramIndex++}`);
+      values.push(endDate);
+    }
+    if (search) {
+      whereConditions.push(
+        `(customer.name ILIKE $${paramIndex} OR th.code ILIKE $${paramIndex})`,
+      );
+      values.push(`%${search}%`);
+      paramIndex++;
+    }
 
-  //   if (sort === TransactionInHeaderSort.IS_CHARGE) {
-  //     sortBy = `transaction_in.${sort}`;
-  //   }
+    const whereClause =
+      whereConditions.length > 0
+        ? `WHERE ${whereConditions.join(' AND ')}`
+        : '';
 
-  //   const queryBuilder = this.transactionInHeaderRepository
-  //     .createQueryBuilder('transaction-header')
-  //     .leftJoinAndSelect('transaction-header.customer', 'customer')
-  //     .leftJoinAndSelect('transaction-header.transaction_in', 'transaction_in')
-  //     .skip(skip)
-  //     .take(pageSize)
-  //     .select([
-  //       'transaction-header',
-  //       'transaction_in',
-  //       'customer.name',
-  //       'customer.id',
-  //     ])
-  //     .orderBy(sortBy, order.toUpperCase() as SortOrderQueryBuilder);
+    const customerColumnsToSelect = [CustomersColumn.ID, CustomersColumn.NAME]
+      .map((col) => `'${col}', customer.${col}`)
+      .join(', ');
+    const transInColumnsToSelect = [TransactionInsColumn.ID]
+      .map((col) => `'${col}', ti.${col}`)
+      .join(', ');
 
-  //   // Conditionally add filters
-  //   if (startDate) {
-  //     queryBuilder.andWhere({ created_at: MoreThanOrEqual(startDate) });
-  //   }
+    const getTransInDetailSql = `
+      SELECT th.*, (jsonb_agg( jsonb_build_object (${customerColumnsToSelect}) )) -> 0 as customer, (jsonb_agg( jsonb_build_object (${transInColumnsToSelect}) )) -> 0 as transaction_ins
+      FROM ${DATABASE.TRANSACTION_IN_HEADER} as th
+      LEFT JOIN ${DATABASE.CUSTOMERS} customer on th.customerid = customer.id
+      LEFT JOIN ${DATABASE.TRANSACTION_INS} ti on ti.transaction_in_headerId = th.id 
+      ${whereClause}
+      GROUP BY th.id
+      ORDER BY ${sortBy} ${order}
+      LIMIT $${paramIndex++}
+      OFFSET $${paramIndex++}
+      `;
 
-  //   if (endDate) {
-  //     queryBuilder.andWhere({ created_at: LessThan(endDate) });
-  //   }
+    const paginationCountSql = `
+      SELECT count(*) as total_count
+      FROM ${DATABASE.TRANSACTION_IN_HEADER} as th
+      LEFT JOIN ${DATABASE.CUSTOMERS} customer on th.customerid = customer.id
+      LEFT JOIN ${DATABASE.TRANSACTION_INS} ti on ti.transaction_in_headerId = th.id 
+      ${whereClause}
+                  `;
 
-  //   if (search) {
-  //     queryBuilder.andWhere(
-  //       new Brackets((qb) => {
-  //         qb.where('customer.name LIKE :search', { search: `%${search}%` });
-  //         qb.where('transaction-header.code LIKE :search', {
-  //           search: `%${search}%`,
-  //         });
-  //       }),
-  //     );
-  //   }
+    values.push(pageSize, (pageNo - 1) * pageSize);
 
-  //   const [transactionsIns, count] = await queryBuilder.getManyAndCount();
-  //   return [transactionsIns, count];
-  // }
+    try {
+      const { rows: transDetailRows } =
+        await this.pool.query<TransactionInHeader>(getTransInDetailSql, values);
+      const { rows: totalCountRows } = await this.pool.query<{
+        total_count: string;
+      }>(
+        paginationCountSql,
+        whereConditions.length != 0 ? values.slice(0, -2) : [],
+      );
+      const totalCount = parseInt(totalCountRows[0].total_count, 10);
 
-  // async findTransactionInHeaderById(id: number) {
-  //   const transactionIn = await this.transactionInHeaderRepository.findOne({
-  //     where: { id },
-  //     relations: ['customer', 'transaction_in', 'transaction_in.product'],
-  //   });
-  //   if (!transactionIn)
-  //     throw new NotFoundException('No Transaction In Header with that id');
-  //   return transactionIn;
-  // }
+      return [transDetailRows, totalCount];
+    } catch (error) {
+      console.error('Failed to get all Transaction Ins:', error);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async findTransactionInHeaderById(id: number) {
+    const customerColumnsToSelect = [CustomersColumn.ID, CustomersColumn.NAME]
+      .map((col) => `'${col}', c.${col}`)
+      .join(', ');
+    const transInColumnsToSelect = [TransactionInsColumn.ID]
+      .map((col) => `'${col}', ti.${col}`)
+      .join(', ');
+
+    const sql = `
+      SELECT th.*, (jsonb_agg( jsonb_build_object (${customerColumnsToSelect}) )) -> 0 as customer, (jsonb_agg( jsonb_build_object (${transInColumnsToSelect}, 'product',jsonb_build_object('id',p.id,'name',p.name) ) )) -> 0 as transaction_ins
+      FROM ${DATABASE.TRANSACTION_IN_HEADER} th
+      LEFT JOIN ${DATABASE.CUSTOMERS} c on c.id = th.customerid
+      LEFT JOIN ${DATABASE.TRANSACTION_INS} ti on ti.transaction_in_headerid = th.id
+      LEFT JOIN ${DATABASE.PRODUCTS} p on p.id = ti.productid
+      WHERE th.id = $1
+      GROUP BY th.id
+    `;
+    console.log(sql);
+    const { rows } = await this.pool.query(sql, [id]);
+    if (rows.length === 0)
+      throw new NotFoundException('No Transaction In Header with that id');
+    return rows;
+  }
 
   // async getAllTransactionInHeadersByCustomerId(
   //   customerId: number,
